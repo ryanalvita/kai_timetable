46f61047e81e5d482711d0461d5d07c418ad19b0
ryanalvita <ryanalvita@yahoo.com>
2025-03-15 17:25:01 +0100

    Add workflows and make it possible with inputs

--- scrape_pdf.py
+++ scrape_pdf.py
@@ -1,43 +1,41 @@
+import sys
 import camelot
 import pandas as pd
-
 from constants import kanumber_page_mapping, filepath
 
-for kanumbers, pages in kanumber_page_mapping.items():
-    page_start, page_end = pages
-    pages = ",".join([str(i) for i in range(page_start, page_end + 1)])
+# Parse command-line arguments.
+# Expected usage: python scrape_pdf.py <start_index> <end_index>
+if len(sys.argv) >= 3:
+    start_index = int(sys.argv[1])
+    end_index = int(sys.argv[2])
+else:
+    start_index = None
+    end_index = None
 
-    tables = camelot.read_pdf(filepath=filepath, pages=pages)
+# Filter the mapping based on the provided indices.
+if start_index is not None and end_index is not None:
+    filtered_mapping = {}
+    for k, pages in kanumber_page_mapping.items():
+        try:
+            key_int = int(k)
+        except ValueError:
+            continue  # Skip keys that can't be converted to an integer.
+        if start_index <= key_int <= end_index:
+            filtered_mapping[k] = pages
+else:
+    filtered_mapping = kanumber_page_mapping
+
+# Process each filtered mapping.
+for kanumber, pages in filtered_mapping.items():
+    page_start, page_end = pages
+    pages_str = ",".join(str(i) for i in range(page_start, page_end + 1))
+
+    tables = camelot.read_pdf(filepath=filepath, pages=pages_str)
 
     df = pd.DataFrame()
     for table in tables:
         df = pd.concat([df, table.df])
-    df.to_csv(f"page/page{page_start}-{page_end}.csv")
 
-# # Cleanup for routes
-# df = df.drop(0, axis=1)
-# df = df.drop(7, axis=1)
-
-# columns = {
-#     1: "train_number",
-#     2: "train_class",
-#     3: "train_name",
-#     4: "train_route",
-#     5: "departure_time",
-#     6: "arrival_time",
-# }
-
-# df = df.rename(columns=columns)
-
-# # Parse route
-# df[["origin_station", "destination_station"]] = df["train_route"].str.split(
-#     "-", expand=True
-# )
-# df = df.drop("train_route", axis=1)
-# df = df.drop("index", axis=1)
-
-# df = df.reset_index()
-
-# df.to_csv("routes.csv")
-
-# a = 1
+    output_filename = f"page/page{page_start}-{page_end}.csv"
+    df.to_csv(output_filename, index=False)
+    print(f"Saved {output_filename}")
